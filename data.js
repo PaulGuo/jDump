0e9e0fb7a859ba0b810c34e365d7a2beafc2199a
GuoKai <badkaikai@gmail.com>
2012-08-23 15:39:28 +0800

    added files.

---
+++ data.js
@@ -0,0 +1,39 @@
+var data = {
+
+    // a list of user profiles
+    users:[
+        { first: "John", last: "Smith", age: 45, admin: false, locationId:2, gender: "m", permissions: ["read"] },
+        { first: "Margret", last: "Lynn", age: 32, admin: true, locationId:1, gender: "f", permissions: ["read", "write"] },
+        { first: "Ryan", last: "Aston", age: 22, admin: false, locationId:3, gender: "M", permissions: ["read", "write"] },
+        { first: "Zoe", last: "Brown", age: 21, admin: true, locationId:2, gender: "f", permissions: ["read"] },
+        { first: "Haley", last: "Baker", age: 14, admin: true, locationId:3, gender: "f", permissions: ["read", "write", "delete"] },
+        { first: "Chase", last: "Herbert", age: 17, admin: false, locationId:7, gender: "m", permissions: ["read"] },
+        { first: "Mary", last: "Godard", age: 65, admin: true, locationId:2, gender: "f", permissions: ["read"] },
+        { first: "Oliva", last: "Sellick", age: 44, admin: true, locationId:6, gender: "f", permissions: ["read"] },
+        { first: "Justin", last: "Stanwood", age: 21, admin: true, locationId:5, gender: "M", permissions: ["READ"] },
+        { first: "Adam", last: "Smith", age: 45, admin: false, locationId:5, gender: "m", permissions: ["read"] },
+        { first: "Audrey", last: "Worth", age: 32, admin: false, locationId:2, gender: "f", permissions: ["read"] },
+        { first: "Owen", last: "Walter", age: 61, admin: false, locationId:1, gender: "m", permissions: ["read"] },
+        { first: "Seth", last: "Morgan", age: 45, admin: false, locationId:2, gender: "m", permissions: ["read", "write"] },
+        { first: "Carter", last: "Fry", age: 38, admin: false, locationId:3, gender: "m", permissions: ["read", "write"] },
+        { first: "Justin", last: "Cromwell", age: 25, admin: true, locationId:4, gender: "M", permissions: ["read"] },
+        { first: "Brian", last: "Martin", age: 32, admin: false, locationId:7, gender: "m", permissions: ["read"] },
+        { first: "Ava", last: "Barton", age: 14, admin: false, locationId:7, gender: "f", permissions: ["READ", "WRITE", "delete"] },
+        { first: "David", last: "Baum", age: 18, admin: false, locationId:2, gender: "m", permissions: ["read"] },
+        { first: "Daniel", last: "Ashford", age: 56, admin: false, locationId:2, gender: "m", permissions: ["read", "write", "delete"] },
+        { first: "Lilly", last: "Edwin", age: 73, admin: true, locationId:3, gender: "f", permissions: ["read", "write"] },
+        { first: "Logan", last: "Lee", age: 23, admin: false, locationId:4, gender: "m", permissions: ["read", "write"] },
+        { first: "James", last: "Haff", age: 24, admin: false, locationId:4, gender: "m", permissions: ["read"] },
+        { first: "Zach", last: "Lane", age: 67, admin: false, locationId:1, gender: "m", permissions: ["read"] },
+        { first: "Abby", last: "Bartin", age: 73, admin: false, locationId:7, gender: "F", permissions: ["read"] },
+        { first: "Paige", last: "Darrow", age: 38, admin: true, locationId:6, gender: "f", permissions: ["read"] },
+        { first: "Matt", last: "Raymond", age: 12, admin: false, locationId:7, gender: "m", permissions: ["read", "write"] },
+        { first: "Daniel", last: "Oswald", age: 29, admin: true, locationId:4, gender: "m", permissions: ["READ", "WRITE"] },
+        { first: "Abigail", last: "Weller", age: 40, admin: false, locationId:5, gender: "f", permissions: ["READ", "WRITE", "DELETE"] },
+        { first: "Madison", last: "Smith", age: 21, admin: false, locationId:3, gender: "F", permissions: ["read"] },
+        { first: "Emma", last: "Yager", age: 20, admin: true, locationId:1, gender: "f", permissions: ["read", "write"] },
+        { first: "Luke", last: "Heaton", age: 16, admin: false, locationId:2, gender: "m", permissions: ["read"] },
+        { first: "Chris", last: "Everard", age: 34, admin: false, locationId:4, gender: "m", permissions: ["read"] }
+    ]
+
+};
